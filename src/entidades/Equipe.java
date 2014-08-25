552ea006c230714f37fef45ff27d83ea48582e4b
Jonathan <jonathan.correa@biostore.com.br>
2014-08-24 22:34:31 -0300

    equipe

--- src/entidades/Equipe.java
+++ src/entidades/Equipe.java
@@ -5,6 +5,9 @@
  */
 package entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Jonathan
@@ -12,12 +15,15 @@ package entidades;
 public class Equipe {
 
     // atributos
-    private String EquipeID;
+    private int equipeID;
     private String nome;
 
     // construtor
     public Equipe(String nome) {
         this.nome = nome;
+    }
+
+    public Equipe() {
     }
 
     // gets e sets
@@ -27,5 +33,42 @@ public class Equipe {
 
     public String getNome() {
         return nome;
+    }
+
+    public void setEquipeID(int equipeID) {
+        this.equipeID = equipeID;
+    }
+
+    public int getEquipeID() {
+        return equipeID;
+    }
+
+    public static List getEquipes() throws Exception {
+        try {
+            ArrayList list = new ArrayList();
+
+            entidades.Equipe equipe = new entidades.Equipe();
+            equipe.setEquipeID(new Integer(1));
+            equipe.setNome("Fornecedor 1");
+            list.add(equipe);
+
+            equipe = new entidades.Equipe();
+            equipe.setEquipeID(new Integer(2));
+            equipe.setNome("Fornecedor 2");
+            list.add(equipe);
+
+            equipe = new entidades.Equipe();
+            equipe.setEquipeID(new Integer(3));
+            equipe.setNome("Fornecedor 3");
+            list.add(equipe);
+
+            return list;
+//            String insert = "INSERT INTO Equipe (Nome) VALUES ('" + equipe.getNome() + "')";
+
+//            AcessoBanco bd = new AcessoBanco();
+//            bd.executar(insert);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
